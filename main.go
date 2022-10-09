5b93a132883f0a2d3b34e12917b61f134e266b4f
GotoRen <r0719en@pluslab.org>
2022-10-09 21:25:50 +0900

    🎉 [Add] First commit

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	var netInterface *net.Interface
 	var err error
 
-	// interf = "en0"
-	interf = "eth0"
+	interf = "en0"
+	// interf = "eth0"
 	netInterface, err = checkInterface(interf)
 	if err != nil {
 		fmt.Println(err)
@@ -19,7 +19,7 @@ func main() {
 	getHardwareAddr(netInterface)
 
 	// interf = "en10"
-	interf = "eth1"
+	// interf = "eth1"
 	netInterface, err = checkInterface(interf)
 	if err != nil {
 		fmt.Println(err)
@@ -29,4 +29,35 @@ func main() {
 	// fmt.Println(netInterface.HardwareAddr)
 
 	getHardwareAddr(netInterface)
+}
+
+// checkInterface returns the obtained interface information
+func checkInterface(interf string) (netInterface *net.Interface, err error) {
+	for {
+		netInterface, err = net.InterfaceByName(interf)
+		if err != nil {
+			fmt.Println("Can't find interface")
+		} else {
+			break
+		}
+	}
+	fmt.Println("Connected.")
+
+	return netInterface, nil
+}
+
+// getHardwareAddr retruns extract the hardware information base on the interface name
+func getHardwareAddr(netInterface *net.Interface) {
+	name := netInterface.Name
+	macAddress := netInterface.HardwareAddr
+
+	fmt.Println("Hardware name :", name)
+	fmt.Println("MAC address :", macAddress)
+
+	// verify if the MAC address can be parsed properly
+	hwAddr, err := net.ParseMAC(macAddress.String())
+	if err != nil {
+		fmt.Println("No able to parse MAC address :", err)
+	}
+	fmt.Printf("Physical hardware address :%s \n", hwAddr.String())
 }
